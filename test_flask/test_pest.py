59ad1beef5b0684c4a2e38e7661fb52ab03f6f91
nazarKobryn <kobrinnazar05@gmail.com>
2024-01-15 11:18:50 +0200

    added test_pest

--- test_flask/test_pest.py
+++ test_flask/test_pest.py
@@ -4,20 +4,47 @@ from click.testing import Result
 from app import models as m, db
 from test_flask.utils import login
 
+TEST_NAME = "test_pets"
+
 
 def test_pest_crud(login_client: FlaskClient):
-    data = dict(
-        name="test_pest",
+    pest_data = dict(
+        name=TEST_NAME,
         symptoms="test_symptoms",
         treatment="test_treatment",
     )
     response = login_client.post(
         "/pest/create",
-        data=data,
+        data=pest_data,
     )
-    assert response.status_code == 201
-    pest_rows_objs = db.session.execute(m.Pest.select()).all()
-    assert len(pest_rows_objs) > 0
+    assert response.status_code == 302
+    pest: m.Pest = db.session.scalars(m.Pest.select()).first()
+    assert pest
+
+    # test updated
+    NEW_NAME = "some name"
+    pest_data["pest_id"] = str(pest.id)
+    pest_data["name"] = NEW_NAME
+    response = login_client.post("/pest/save", data=pest_data, follow_redirects=True)
+    assert response.status_code == 200
+    assert NEW_NAME.encode("utf-8") in response.data
+
+    pest_data["pest_id"] = "100"
+    response = login_client.post("/pest/save", data=pest_data, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Cannot save pest data" in response.data
+
+    # test delete
+    response = login_client.delete(
+        f"/pest/delete/{pest.id}", data=pest_data, follow_redirects=True
+    )
+    assert response.status_code == 200
+    assert b"ok" in response.data
+    response = login_client.delete(
+        f"/pest/delete/{pest.id}", data=pest_data, follow_redirects=True
+    )
+    assert response.status_code == 404
+    assert b"no pest" in response.data
 
 
 def test_populate_db(runner: FlaskCliRunner):
