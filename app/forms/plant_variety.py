df065423dfa301a75fe3524ff21cee0d4905e58b
nazarKobryn <kobrinnazar05@gmail.com>
2024-01-17 11:44:12 +0200

    updated form

--- app/forms/plant_variety.py
+++ app/forms/plant_variety.py
@@ -17,16 +17,16 @@ class MultiCheckboxField(SelectMultipleField):
 
 class PlantVarietyForm(FlaskForm):
     name = StringField("name", [DataRequired(), Length(1, 64)])
-    features = StringField("features", [Length(0, 1024)])
-    general_info = StringField("general_info", [Length(0, 2048)])
-    temperature_info = StringField("temperature_info", [Length(0, 2048)])
-    watering_info = StringField("watering_info", [Length(0, 2048)])
+    features = StringField("features", [Length(0, 1024)], default="")
+    general_info = StringField("general_info", [Length(0, 2048)], default="")
+    temperature_info = StringField("temperature_info", [Length(0, 2048)], default="")
+    watering_info = StringField("watering_info", [Length(0, 2048)], default="")
     planting_min_temperature = FloatField("planting_min_temperature")
     planting_max_temperature = FloatField("planting_max_temperature")
     is_moisture_loving = BooleanField("is_moisture_loving")
     is_sun_loving = BooleanField("is_sun_loving")
     ground_ph = FloatField("ground_ph")
-    ground_type = StringField("ground_type")
+    ground_type = StringField("ground_type", [Length(0, 256)], default="")
     can_plant_indoors = BooleanField("can_plant_indoors")
 
     pests = MultiCheckboxField("pests", choices=[], validate_choice=False)
